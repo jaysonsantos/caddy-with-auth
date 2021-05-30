8695bbed576dd5838e920e30da04cac2048d0349
Jayson Reis <santosdosreis@gmail.com>
2021-05-30 20:15:07 +0200

    Enable standard modules

--- main.go
+++ main.go
@@ -28,6 +28,8 @@ package main
 
 import (
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
+	_ "github.com/caddyserver/caddy/v2/modules/standard"
+
 	_ "github.com/greenpau/caddy-auth-jwt"
 	_ "github.com/greenpau/caddy-auth-portal"
 )
